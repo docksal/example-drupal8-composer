57c5e26c00c4204baac3d2fdeda49e10bd748166
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-12-08 22:33:58 +0100

    Add drush alias

---
+++ drush/demo-drupal.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+
+$aliases['master'] = array (
+'root' => '/var/www/web',
+'uri' => 'http://master.demo-drupal.dev.andock.ci',
+'remote-user' => 'mp4yqom',
+'remote-host' => 'dev.andock.ci',
+'ssh-options' => '-p 2222'
+);
