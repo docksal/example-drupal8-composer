934155597481874b2460472739377e1ef5097c23
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-12-13 00:41:37 +0100

    Rename drush file.

--- web/drush/demo.aliases.drushrc.php
+++ web/drush/demo.aliases.drushrc.php
@@ -2,8 +2,8 @@
 
 $aliases['master'] = array (
 'root' => '/var/www/web',
-'uri' => 'http://master.demo-drupal.dev.andock.ci',
 'remote-user' => 'mp4yqom',
-'remote-host' => 'dev.andock.ci',
+'remote-host' => 'master.demo-drupal.dev.andock.ci',
 'ssh-options' => '-p 2222'
 );
+
